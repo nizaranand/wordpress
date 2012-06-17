01e8f15209480e9f932b9a5b337b068409a64f6f
Anthony Colas <anthony.colas@gmail.com>
2012-06-17 17:56:47 +0200

    improve search

--- wp-content/themes/theme-blandine-0.3/search.php
+++ wp-content/themes/theme-blandine-0.3/search.php
@@ -17,11 +17,11 @@ get_header(); ?>
     <!-- Masthead
       ================================================== -->
       <header class="jumbotron subhead" id="overview">
-        <h1><?php printf( __( 'Search Results for: %s', 'bootstrapwp' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+        <h1><?php printf( __( 'Résultats pour la recherche: %s', 'bootstrapwp' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
        
       </header>
-			 <div class="row content">
-<div class="span8">
+              <div class="row content">
+<div class="span9 hero-unit-page">
 					<?php while ( have_posts() ) : the_post(); ?>
 <h2><?php the_title();?></h2>
 <p><?php the_excerpt();?></p>
@@ -31,12 +31,12 @@ get_header(); ?>
 			<?php else : ?>
  <!-- Masthead
       ================================================== -->
+
+
       <header class="jumbotron subhead" id="overview">
-        <h1><?php _e( 'No Results Found', 'bootstrapwp' ); ?></h1>
-      <p class="lead"><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps you should try again with a different search term.', 'bootstrapwp' ); ?></p>
+        <h1><?php _e( 'Aucun résultat', 'bootstrapwp' ); ?></h1>
+     <p class="lead"><?php _e( 'Il semble que la page que vous recherchez n&rsquo;existe plus. Vous pouvez vous aider de la recherche ou du plan du site ci-dessous :', 'bootstrapwp' ); ?></p>
       </header>
-			 <div class="row content">
-<div class="span8">
 					
 
 <div class="well">
